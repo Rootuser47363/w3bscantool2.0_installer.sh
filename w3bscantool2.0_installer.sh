950fc37376095e085689a59ee2f2f3fcf6ee0f1d
Root/user47363/ <82620412+Rootuser47363@users.noreply.github.com>
2023-03-23 23:11:39 -0600

    Update w3bscantool2.0_installer.sh

--- w3bscantool2.0_installer.sh
+++ w3bscantool2.0_installer.sh
@@ -20,14 +20,25 @@ if [ -f /etc/debian_version ]; then
   # Debian, Ubuntu
   echo "Detectado Debian/Ubuntu"
   sudo apt-get update
-  sudo apt-get install -y curl
 elif [ -f /etc/redhat-release ]; then
-  # Red Hat, CentOS
-  echo "Detectado Red Hat/CentOS"
-  sudo yum update
-  sudo yum install -y curl
+  # Red Hat, CentOS, Fedora
+  echo "Detectado Red Hat/CentOS/Fedora"
+  sudo dnf update
+elif [ -f /etc/arch-release ]; then
+  # Arch Linux
+  echo "Detectado Arch Linux"
+  sudo pacman -Sy
 else
   echo "Sistema operativo no soportado"
   exit 1
 fi
 
+# Descargar script desde Github
+echo "Descargando script desde Github..."
+curl -O https://raw.githubusercontent.com/Rootuser47363/w3bscantool2.0.sh/main/w3bscantool2.0.sh
+
+# Dar permisos de ejecución
+chmod +x w3bscantool2.0.sh
+
+echo "La instalación de w3bscantool2.0.sh se ha completado exitosamente."
+echo "Para ejecutar la herramienta, use el comando: ./w3bscantool2.0.sh"
